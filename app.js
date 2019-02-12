93ac948b66e0816bf6e7a6c7840dff1d31c3c692
Maribel <minicatsCB@users.noreply.github.com>
2019-02-12 12:10:18 +0100

    Implement movie save

--- app.js
+++ app.js
@@ -41,6 +41,39 @@ function replaceNullData(strings, ...parts) {
     return checkedMarkup + strings[strings.length - 1];
 }
 
+function saveMovie() {
+    // Test data
+    var movieData = {
+        title: "Bond",
+        genre: "Action"
+    };
+
+    checkMovieExists(movieData.title).then(movieExists => {
+        if(movieExists) {
+            console.log("Cannot add the movie. The movie already exists in the database.");
+        } else {
+            console.log("Adding movie to the database.");
+            database.ref().child("movies").push(movieData);
+        }
+    });
+}
+
+function checkMovieExists(title) {
+    let movieExists = false;
+    return database.ref().child("movies")
+        .orderByChild("title")
+        .equalTo(title)
+        .once("value")
+        .then(snapshot => {
+            snapshot.forEach((childSnapshot) => {
+                // If a movie was returned, then we know it already exists in the database
+                movieExists = true;
+            });
+
+            return movieExists;
+    });
+}
+
 function initDatabase() {
     var config = {
         apiKey: "yourApiKey",
@@ -55,3 +88,5 @@ function initDatabase() {
 
     return firebase.database();
 }
+
+let database = initDatabase();
