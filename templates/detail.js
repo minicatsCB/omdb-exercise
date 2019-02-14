d4a745e50366e552f5dfa365fc5221ae20ef3d83
Maribel <minicatsCB@users.noreply.github.com>
2019-02-14 19:12:03 +0100

    Minor visual enhancements

--- templates/detail.js
+++ templates/detail.js
@@ -2,51 +2,47 @@ function createDetailTemplate (movieValue) {
     let detailsTemplate = `
         <div class="movie-details col-lg-9">
             <div>
-                <h1>${movieValue.Title}</h1>
-            </div>
-
-            <div>
                 <div class="row">
                     <div class="col-lg-3 col-md-4 intro-section">
                         <div class="info-item">
-                            <span>Language</span>
+                            <span><b>Language</b></span>
                             <input type="text" class="form-control-plaintext" itemprop="Language" value="${movieValue.Language}" disabled>
                         </div>
 
                         <div class="info-item">
-                            <span>Director</span>
+                            <span><b>Director</b></span>
                             <input type="text" class="form-control-plaintext" itemprop="Director" value="${movieValue.Director}" disabled>
                         </div>
 
                         <div class="info-item">
-                            <span>Country</span>
+                            <span><b>Country</b></span>
                             <input type="text" class="form-control-plaintext" itemprop="Country" value="${movieValue.Country}" disabled>
                         </div>
 
                         <div class="info-item">
-                            <span>Release year</span>
+                            <span><b>Release year</b></span>
                             <input type="text" class="form-control-plaintext" itemprop="Year" value="${movieValue.Released}" disabled>
                         </div>
 
                         <div class="info-item">
-                            <span>Genres</span>
+                            <span><b>Genres</b></span>
                             <input type="text" class="form-control-plaintext" itemprop="Genre" value="${movieValue.Genre}" disabled>
                         </div>
 
                         <div class="info-item">
-                            <span>Runtime</span>
+                            <span><b>Runtime</b></span>
                             <input type="text" class="form-control-plaintext" itemprop="Runtime" value="${movieValue.Runtime}" disabled>
                         </div>
 
                         <div class="info-item">
-                            <span>Rated</span>
+                            <span><b>Rated</b></span>
                             <input type="text" class="form-control-plaintext" itemprop="Rated" value="${movieValue.Rated}" disabled>
                         </div>
                     </div>
 
                     <div class=" col-lg-6 col-md-8 sinopsis-section">
                         <div class="info-item">
-                            <span>Plot summary</span>
+                            <span><b>Plot summary</b></span>
                             <input type="text" class="form-control-plaintext" itemprop="Plot" value="${movieValue.Plot}" disabled>
                         </div>
                     </div>
@@ -54,13 +50,13 @@ function createDetailTemplate (movieValue) {
                     <div class="col-lg-3 col-md-12 extras-section">
                         <div class="info-item">
                             <i class="fas fa-award"></i>
-                            <span>Awards</span>
+                            <span><b>Awards</b></span>
                             <input type="text" class="form-control-plaintext" itemprop="Awards" value="${movieValue.Awards}" disabled>
                         </div>
 
                         <div class="info-item">
                             <i class="fas fa-star"></i>
-                            <span>Rating</span>
+                            <span><b>Rating</b></span>
                             <input type="text" class="form-control-plaintext" itemprop="imdbRating" value="${movieValue.imdbRating}" disabled>
                         </div>
                     </div>
--- templates/master.js
+++ templates/master.js
@@ -2,27 +2,28 @@ function createMasterTemplate (movieKey, movieValue) {
     let movieTemplate = `
         <div id="${movieKey}" class="row">
             <div class="col-lg-3">
-                <div class="row">
-                    <div class="movie-poster col-lg-12">
+                <div class="row my-3">
+                    <div class="col-lg-12">
+                        <h1>${movieValue.Title}</h1>
                         <img class="w-100" src="${movieValue.Poster}" alt="movie_poster">
                     </div>
                     <div class="movie-options col-lg-12" data-key="${movieKey}">
-                        <div>
-                            <button class="btn btn-primary w-100" type="button" itemprop="details">
+                        <div class="my-1">
+                            <button class="btn btn-light w-100" type="button" itemprop="details">
                                 <i class="fas fa-info-circle"></i>
                                 <span>View details</span>
                             </button>
                         </div>
 
-                        <div>
-                            <button class="btn btn-primary w-100" type="button" itemprop="edit">
+                        <div class="my-1">
+                            <button class="btn btn-light w-100" type="button" itemprop="edit">
                                 <i class="fas fa-edit"></i>
                                 <span>Edit</span>
                             </button>
                         </div>
 
-                        <div>
-                            <button class="btn btn-primary w-100" type="button" itemprop="delete">
+                        <div class="my-1">
+                            <button class="btn btn-light w-100" type="button" itemprop="delete">
                                 <i class="fas fa-trash-alt"></i>
                                 <span>Delete</span>
                             </button>
